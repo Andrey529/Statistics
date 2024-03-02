898ae8437ec8d9a7c594f75ed29b0a851c3b8c69
Andrey <75383288+Andrey529@users.noreply.github.com>
2024-03-02 20:18:46 +0300

    done LAB2

--- LAB1/main.py
+++ LAB1/main.py
@@ -1,8 +1,10 @@
+import matplotlib
+matplotlib.use('TkAgg')  # или 'Qt5Agg'
 import matplotlib.pyplot as plt
 import DataGenerator as dataGen
 
 
-def main():
+def draw_0var():
     mu0 = [0, 2, 3]
     mu1 = [3, 5, 1]
     mu = [mu0, mu1]
@@ -13,13 +15,6 @@ def main():
     n = 1000  # число объектов класса
 
     x, y, class0, class1 = dataGen.norm_dataset(mu, sigma, n)
-
-    # разделяем данные на 2 подвыборки
-    trainCount = round(0.7 * n * 2)  # не забываем округлить до целого
-    xtrain = x[0:trainCount]
-    xtest = x[trainCount:n * 2 + 1]
-    ytrain = y[0:trainCount]
-    ytest = y[trainCount:n * 2 + 1]
 
     # построение гистограмм распределения для всех признаков
     for i in range(0, col):
@@ -36,11 +31,8 @@ def main():
     plt.show()
 
 
-if __name__ == "__main__":
-    # main()
-
-    num_points = 300
-    x1, y1, x2, y2 = dataGen.nonlinear_dataset_5var(num_points)
+def draw_data(get_dataset):
+    x1, y1, x2, y2 = get_dataset
 
     # Визуализация данных
     plt.scatter(x1, y1)
@@ -50,3 +42,20 @@ if __name__ == "__main__":
     plt.title('График распределения двух классов случайных чисел')
     plt.grid(True)
     plt.show()
+
+
+def main():
+    print("Введите вариант для генерации данных: ")
+    var = int(input())
+    if var == 0:
+        draw_0var()
+    else:
+        num_points = 300
+        if var == 2:
+            draw_data(dataGen.nonlinear_dataset_2var(num_points))
+        elif var == 5:
+            draw_data(dataGen.nonlinear_dataset_5var(num_points))
+
+
+if __name__ == "__main__":
+    main()
--- Lab2/main.py
+++ Lab2/main.py
@@ -5,14 +5,48 @@ from sklearn.linear_model import LogisticRegression
 import LAB1.DataGenerator as dataGen
 
 
+def draw_plt(data, indexes, title):
+    plt.hist(data[indexes, 0], bins='auto', alpha=0.7)
+    plt.hist(data[~indexes, 0], bins='auto', alpha=0.7)
+    plt.title(title)
+    plt.show()
+
+
+def calculate_accurancy(pred, y):
+    return sum(pred == y) / len(y)
+
+
+def calculate_sensitivity_specificity(pred, y):
+    # Рассчитаем количество TP, FN, TN, FP для выборки
+    TP_test = sum((pred == 1) & (y == 1))
+    FN_test = sum((pred == 0) & (y == 1))
+    TN_test = sum((pred == 0) & (y == 0))
+    FP_test = sum((pred == 1) & (y == 0))
+
+    # Рассчитаем чувствительность и специфичность для выборки
+    sensitivity = TP_test / (TP_test + FN_test)
+    specificity = TN_test / (TN_test + FP_test)
+    return sensitivity, specificity
+
+
 def main():
-    mu0 = [0, 2, 3]
-    mu1 = [3, 5, 1]
+    mode = int(input("Введите вариант генерации данных: "
+                     "0 - линейно разделимые, 1 - нелинейнопересекаемые"))
+    mu0, mu1, sigma0, sigma1 = [], [], [], []
+    if mode == 0:
+        mu0 = [0, 2, 3]
+        mu1 = [8, 13, 6]
+        sigma0 = [2, 1, 2]
+        sigma1 = [1, 2, 1]
+    elif mode == 1:
+        mu0 = [0, 2, 3]
+        mu1 = [3, 5, 1]
+        sigma0 = [2, 1, 2]
+        sigma1 = [1, 2, 1]
+
     mu = [mu0, mu1]
-    sigma0 = [2, 1, 2]
-    sigma1 = [1, 2, 1]
     sigma = [sigma0, sigma1]
-    n = 1000  # число объектов класса
+    n = 1000 # число объектов класса
 
     x, y, class0, class1 = dataGen.norm_dataset(mu, sigma, n)
 
@@ -31,50 +65,29 @@ def main():
     Pred_test = clf.predict(xtest)
     Pred_test_proba = clf.predict_proba(xtest)
 
-    plt.hist(Pred_test_proba[ytest, 0], bins='auto', alpha=0.7)
-    plt.hist(Pred_test_proba[~ytest, 0], bins='auto', alpha=0.7)
-    plt.title("Результаты классификации, тестовая выборка")
-    plt.show()
+    title_test = "Результаты классификации, тестовая выборка"
+    draw_plt(Pred_test_proba, ytest, title_test)
 
     Pred_train = clf.predict(xtrain)
     Pred_train_proba = clf.predict_proba(xtrain)
 
-    plt.hist(Pred_train_proba[ytrain, 0], bins='auto', alpha=0.7)
-    plt.hist(Pred_train_proba[~ytrain, 0], bins='auto', alpha=0.7)
-    plt.title("Результаты классификации, тренировочная выборка")
-    plt.show()
+    title_train = "Результаты классификации, тренировочная выборка"
+    draw_plt(Pred_train_proba, ytrain, title_train)
 
     # acc_train = clf.score(xtrain, ytrain)
     # acc_test = clf.score(xtest, ytest)
 
-    acc_train = sum(Pred_train == ytrain) / len(ytrain)
-    acc_test = sum(Pred_test == ytest) / len(ytest)
-
+    acc_train = calculate_accurancy(Pred_train, ytrain)
+    acc_test = calculate_accurancy(Pred_test, ytest)
     print("acc_train = ", acc_train, ", acc_test = ", acc_test)
 
-    # Рассчитаем количество TP, FN, TN, FP для тестовой выборки
-    TP_test = sum((Pred_test == 1) & (ytest == 1))
-    FN_test = sum((Pred_test == 0) & (ytest == 1))
-    TN_test = sum((Pred_test == 0) & (ytest == 0))
-    FP_test = sum((Pred_test == 1) & (ytest == 0))
-
     # Рассчитаем чувствительность и специфичность для тестовой выборки
-    sensitivity_test = TP_test / (TP_test + FN_test)
-    specificity_test = TN_test / (TN_test + FP_test)
-
+    sensitivity_test, specificity_test = calculate_sensitivity_specificity(Pred_test, ytest)
     print("Sensitivity (Test):", sensitivity_test)
     print("Specificity (Test):", specificity_test)
 
-    # Рассчитаем количество TP, FN, TN, FP для тренировочной выборки
-    TP_train = sum((Pred_train == 1) & (ytrain == 1))
-    FN_train = sum((Pred_train == 0) & (ytrain == 1))
-    TN_train = sum((Pred_train == 0) & (ytrain == 0))
-    FP_train = sum((Pred_train == 1) & (ytrain == 0))
-
     # Рассчитаем чувствительность и специфичность для тренировочной выборки
-    sensitivity_train = TP_train / (TP_train + FN_train)
-    specificity_train = TN_train / (TN_train + FP_train)
-
+    sensitivity_train, specificity_train = calculate_sensitivity_specificity(Pred_train, ytrain)
     print("Sensitivity (Train):", sensitivity_train)
     print("Specificity (Train):", specificity_train)
 
